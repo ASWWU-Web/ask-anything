ef8f9509785338fc0c9c5fb6f2fd9527439bdf46
Sheldon Woodward <sheldonkwoodward@gmail.com>
2017-09-26 19:02:28 -0700

    Added rudimentary post for addind a question.

--- app/routes/home/home.component.ts
+++ app/routes/home/home.component.ts
@@ -1,12 +1,28 @@
 import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { RequestService } from '../../RequestService/request.service';
+
 @Component({
   selector: 'home',
   templateUrl: 'app/routes/home/home.component.html',
-  styleUrls:  ['app/routes/home/home.component.css']
+  styleUrls:  ['app/routes/home/home.component.css'],
+  providers: [ RequestService ]
 })
 
 export class HomeComponent {
+  question: string = "";
 
+  constructor(private rs: RequestService) {
+
+  }
+
+  submit() {
+    console.log(this.question);
+    this.rs.postxwww("/askanything/add", {"question": this.question}, (data) => {
+      alert(data.status);
+    }, (error) => {
+      window.alert(error);
+    });
+  }
 }
