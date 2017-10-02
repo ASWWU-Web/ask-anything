23d8240cc0d7abde5296fba3ef7b67d85686c316
Stephen <stephen.ermshar@wallawalla.edu>
2017-10-01 20:24:58 -0700

    styling submit box, footer, and colors

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,6 +7,13 @@ import {Component, NgModule} from '@angular/core';
     <div class="container">
       <router-outlet></router-outlet>
     </div>
+    <nav class="navbar  navbar-dark bg-inverse" style="border-radius:0;">
+    <div class="float-left text-white small">
+        Coded with
+        <font color="red">♥</font> by the <a href="https://aswwu.com" target="_blank">ASWWU</a> web team.
+        <a href="#top" style="float:right;" class="text-white smooth">Back to Top</a>
+    </div>
+    </nav>
   `
 })
 
